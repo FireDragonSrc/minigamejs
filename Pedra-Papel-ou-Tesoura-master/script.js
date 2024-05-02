48138a52355fd3a0c74c0a478c88e072163a7c84
FireDragonSrc <145880212+FireDragonSrc@users.noreply.github.com>
2024-05-01 23:09:14 -0300

    Update script.js

--- Pedra-Papel-ou-Tesoura-master/script.js
+++ Pedra-Papel-ou-Tesoura-master/script.js
@@ -45,7 +45,7 @@ function JogarPapel() {
   }
 }
 function JogarPedra() {
-  jogador1.src = "/image/maos03.png";
+  jogador1.src = "/assets/image/maos03.png";
   if ((jogador2.src = "/assets/image/maos.png")) {
     msgJogador1.innerHTML = "Ganhou!";
     msgJogador2.innerHTML = "Perdeu!";
